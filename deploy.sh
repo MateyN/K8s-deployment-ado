b1eedaf3bb57c3882470554cfa67241c716ea8c9
MateyN <mateynnikolov@gmail.com>
2024-07-10 16:10:38 +0300

    script modifications

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
+
+cyan="\033[1;36m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+red="\033[1;31m"
+reset="\033[0m"
+echo
 cd nginx/
+echo -e "${green}MINIKUBE STARTING...${reset}"
+
 minikube start
+sleep 10
 
+kubens az-pl
+echo -e "${green}SWITCHING TO <NAMESPACE>...${reset}"
+sleep 5
+kubens
+
+echo -e "${green}APPLYING KUBERNETES PODS...${reset}"
 kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
-kubectl apply -f pod.yaml
+kubectl get deploy
+sleep 3
 
+kubectl apply -f service.yaml
+kubectl get svc
+sleep 3
+
+kubectl apply -f pod.yaml
+kubectl get pod
+echo
+echo -e "${green}SUCCESS!${reset}"
+echo
+echo -e "${green}GETTING MINIKUBE'S IP AND PORT...${reset}"
 MINIKUBE_IP=$(minikube ip)
 SERVICE_PORT=$(kubectl get svc test -o jsonpath='{.spec.ports[0].nodePort}')
 
-echo "Application url: -> http://$MINIKUBE_IP:$SERVICE_PORT"
+sleep 30
+echo -e "${green}SETTING UP... (approx.30s)${reset}"
+
+echo -e "${cyan}Application URL:${reset} -> ${green}http://$MINIKUBE_IP:$SERVICE_PORT${reset}"
+
 curl http://$MINIKUBE_IP:$SERVICE_PORT
+echo
+echo -e "${green}BUILT SUCCESSFULLY!${reset}"
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+green="\033[1;32m"
+reset="\033[0m"
+
+echo -e "${green}RUN SCRIPT STARTED!!!${reset}"
 chmod +x deploy.sh
 bash deploy.sh

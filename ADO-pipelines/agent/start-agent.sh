d3d58533ada6e7ef86c1499d2878079a1a675eca
MateyN <mateynnikolov@gmail.com>
2024-07-12 15:44:32 +0300

    edited updated the script to run self-hosted agent & trigger the pipeline

--- ADO-pipelines/agent/start-agent.sh
+++ ADO-pipelines/agent/start-agent.sh
@@ -8,3 +8,25 @@ echo -e "${green}START-AGENT SCRIPT EXECUTED!!!${reset}"
 cd /home/mnikolov/myagent
 # Runs self-hosted-agent-pool agent in ADO
 ./run.sh
+
+organization="mateynikolov530"
+project="mateynikolov530"
+pipelineId="1"  
+personalAccessToken="bdumgvuxsyzckgc2ku5zr3xyzpjhvn3ijv7yy6w6pmtpkr4chvxq"
+
+# Define the REST API URL to trigger the pipeline
+apiUrl="https://dev.azure.com/$organization/$project/_apis/pipelines/$pipelineId/runs?api-version=6.0-preview.1"
+
+# Create a JSON payload for the request
+payload='{
+  "resources": {
+    "repositories": {
+      "self": {
+        "refName": "refs/heads/master"  # Specify the branch you want to trigger
+      }
+    }
+  }
+}'
+
+# Trigger the pipeline using curl
+curl -X POST -H "Content-Type: application/json" -u ":$personalAccessToken" -d "$payload" "$apiUrl"

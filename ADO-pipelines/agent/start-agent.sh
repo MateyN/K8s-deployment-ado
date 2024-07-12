b92460df8a13c1a9f74016cc83afc4bd7a1a26ea
MateyN <mateynnikolov@gmail.com>
2024-07-12 14:19:57 +0300

    script for running the ADO agent added

---
+++ ADO-pipelines/agent/start-agent.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+green="\033[1;32m"
+reset="\033[0m"
+echo
+echo -e "${green}START-AGENT SCRIPT EXECUTED!!!${reset}"
+
+cd /home/mnikolov/myagent
+# Runs self-hosted-agent-pool agent in ADO
+./run.sh

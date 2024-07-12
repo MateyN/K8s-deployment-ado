ce708574768c515dcb9b051197de4b380d3141b2
MateyN <mateynnikolov@gmail.com>
2024-07-12 15:07:15 +0300

    opt

--- ADO-pipelines/agent/start-agent.sh
+++ ADO-pipelines/agent/start-agent.sh
@@ -5,6 +5,6 @@ reset="\033[0m"
 echo
 echo -e "${green}START-AGENT SCRIPT EXECUTED!!!${reset}"
 
-cd /home/mnikolov/myagent
+#cd /home/mnikolov/myagent
 # Runs self-hosted-agent-pool agent in ADO
 ./run.sh

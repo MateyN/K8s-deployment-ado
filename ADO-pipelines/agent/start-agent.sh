249eb5e404471b2fc7176940ea23a1498ee332f9
MateyN <mateynnikolov@gmail.com>
2024-07-18 14:48:59 +0300

    new PAT

--- ADO-pipelines/agent/start-agent.sh
+++ ADO-pipelines/agent/start-agent.sh
@@ -2,7 +2,7 @@
 
 green="\033[1;32m"
 reset="\033[0m"
-
+: <<'END_COMMENT'
 # Function to handle Ctrl-C
 function ctrl_c() {
     echo ""
@@ -11,15 +11,15 @@ function ctrl_c() {
     sudo ./svc.sh stop
     exit 1
 }
-
+END_COMMENT
 echo -e "${green}START-AGENT SCRIPT EXECUTED!!!${reset}"
 
 cd /home/mnikolov/myagent || { echo -e "${red}Error: Failed to change directory.${reset}"; exit 1; }
 
 # Runs self-hosted-agent-pool agent in ADO in background
-./agent.sh &
-agent_pid=$!
-
+./agent.sh
+#agent_pid=$!
+: <<'END_COMMENT'
 # Trap Ctrl-C signal
 trap ctrl_c INT
 
@@ -30,20 +30,18 @@ sleep 30  # Adjust this sleep duration based on how long it takes for the agent 
 # Start the agent service
 sudo ./svc.sh start
 sleep 20
-
-# Check if the agent is still running and listening for jobs
-# Replace this condition with appropriate checks for your agent
-if ps -p $agent_pid > /dev/null; then
-    echo "Agent is running and listening for jobs."
-else
-    echo "Agent failed to start or stopped unexpectedly."
-    exit 1
-fi
+END_COMMENT
+#if ps -p $agent_pid > /dev/null; then
+#    echo "Agent is running and listening for jobs."
+#else
+#    echo "Agent failed to start or stopped unexpectedly."
+#    exit 1
+#fi
 
 organization="mateynikolov530"
 project="mateynikolov530"
 pipelineId="1"
-personalAccessToken="bdumgvuxsyzckgc2ku5zr3xyzpjhvn3ijv7yy6w6pmtpkr4chvxq"
+personalAccessToken="sijnb5ewft44ve3lhtzcfciswey25zijie2jjx4o66yqihpp7bvq"
 
 # Define the REST API URL to trigger the pipeline
 apiUrl="https://dev.azure.com/$organization/$project/_apis/pipelines/$pipelineId/runs?api-version=6.0-preview.1"
@@ -63,8 +61,8 @@ payload='{
 curl -X POST -H "Content-Type: application/json" -u ":$personalAccessToken" -d "$payload" "$apiUrl"
 #sleep 10
 
-cd /home/mnikolov/K8s-deployment-ado/ADO-pipelines
-./run.sh
+#cd /home/mnikolov/K8s-deployment-ado/ADO-pipelines
+#./run.sh
 
 # End of script
 echo "Script completed successfully."

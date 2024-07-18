9ba524ef52ddf9cadf6befe7afa05ff33f3b48e8
MateyN <mateynnikolov@gmail.com>
2024-07-18 11:28:31 +0300

    opt

--- ADO-pipelines/run.sh
+++ ADO-pipelines/run.sh
@@ -4,10 +4,10 @@ green="\033[1;32m"
 reset="\033[0m"
 echo
 echo -e "${green}RUN SCRIPT STARTED!!!${reset}"
-cd /home/mnikolov/K8s-deployment-ado/ADO-pipelines/agent/ || { echo -e "${red}Error: Failed to change directory.${reset}"; exit 1; }
+#cd /home/mnikolov/K8s-deployment-ado/ADO-pipelines/agent/ || { echo -e "${red}Error: Failed to change directory.${reset}"; exit 1; }
 
-./start-agent.sh
-sleep 20
+#./start-agent.sh
+#sleep 20
 #echo "Crash happens here!"
 pwd
 cd /home/mnikolov/ADO-pipelines/ADO-pipelines/nginx/ || { echo -e "${red}Error: Failed to change directory.${reset}"; exit 1; }

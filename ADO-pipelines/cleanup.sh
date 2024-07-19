bf507635776c07191337f2780a877fda3ec13ad4
MateyN <mateynnikolov@gmail.com>
2024-07-19 17:06:27 +0300

    Added agent kill in cleanup script && reversed commit

--- ADO-pipelines/cleanup.sh
+++ ADO-pipelines/cleanup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ -f /tmp/agent.pid ]; then
+    agent_pid=$(cat /tmp/agent.pid)
+
+    kill $agent_pid
+
+    rm /tmp/agent.pid
+
+    echo "Agent process stopped and PID file cleaned up."
+else
+    echo "No PID file found. The agent may not be running."
+fi
+
 kubectl get pods
 kubectl delete pod msql && sleep 5
 kubectl get deploys && sleep 5
@@ -7,7 +19,7 @@ kubectl delete deploy msql && sleep 5
 kubens az-pl && sleep 5
 kubectl get pods && sleep 5
 kubectl delete pod nginx && sleep 5
-kubectl get deploys && sleep 5
+kubectl get deploy && sleep 5
 kubectl delete deploy nginx && sleep 5
 
 minikube stop
